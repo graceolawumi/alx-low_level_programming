97d6a3f1b6728e333632f6cd748b9e463d1df5a9
Grace Olawumi <gracewumi26@gmail.com>
2023-04-26 03:17:59 -0700

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints the alphabet in lowercase
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char x;
+
+	for (x = 'a'; x <= 'z'; x++)
+		putchar(x);
+	putchar('\n');
+
+	return (0);
+}
