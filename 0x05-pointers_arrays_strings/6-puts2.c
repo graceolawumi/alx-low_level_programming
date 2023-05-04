47a04268c2cf3da1d6505f64c264b254b6e3a2ea
Grace Olawumi <gracewumi26@gmail.com>
2023-05-03 21:48:51 -0700

    Task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * puts2 - prints every other character of a string
+ * @str: pointer
+ * Return: 0
+ */
+
+void puts2(char *str)
+{
+	int x;
+
+	for (x = 0; str[x] != '\0'; x++)
+		if (x % 2 == 0)
+			_putchar(str[x]);
+	_putchar('\n');
+}
