83477c6bc3ccabd223049ceb01339e592febccde
Grace Olawumi <gracewumi26@gmail.com>
2023-05-07 03:52:55 -0700

    Task 5. Always look up

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * string_toupper - changes all lowercase letters of a string to uppercase
+ * @x: char
+ * Return: x
+ */
+
+char *string_toupper(char *x)
+{
+	int y;
+
+	y = 0;
+	while (x[y] != '\0')
+	{
+		if (x[y] >= 'a' && x[y] <= 'z')
+			x[y] = x[y] - 32;
+		y++;
+	}
+	return (x);
+}
