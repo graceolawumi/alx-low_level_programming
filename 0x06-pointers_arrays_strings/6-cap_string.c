c0e1d037716a1e75fd47e9106ce4e193894f1ff5
Grace Olawumi <gracewumi26@gmail.com>
2023-05-08 07:45:55 -0700

    Task 6 Correction

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -14,7 +14,7 @@ char *cap_string(char *s)
 		while (!(s[l] >= 'a' && s[l] <= 'z'))
 			l++;
 
-		if (s[l - 1] == '' ||
+		if (s[l - 1] == ' ' ||
 				s[l - 1] == '\t' ||
 				s[l - 1] == '\n' ||
 				s[l - 1] == ',' ||
